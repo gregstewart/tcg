9b38363e2e952b8a18d13b1973a69424567493b7
Greg Stewart <gregs@tcias.co.uk>
2015-05-04 23:13:17 +0100

    Start of migrating livescript to es6

---
+++ arch-app/app/routes/not-found.js
@@ -0,0 +1,18 @@
+import BaseRoute from './base-route';
+import arch from 'arch';
+import Layout from '../components/layout'
+
+let layout = arch.dom(Layout());
+let d = arch.DOM;
+
+class NotFoundRoute extends BaseRoute {
+  getTitle() {
+    return super.getTitle('Not Found');
+  }
+
+  static render() {
+    return layout(d.h1('Page Not Found'));
+  }
+}
+
+export default NotFoundRoute;
