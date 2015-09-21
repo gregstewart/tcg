7799633c1a5a95c796a49f50c4f042fdba9f0824
Greg Stewart <gregs@tcias.co.uk>
2015-09-22 00:12:20 +0100

    Added counter examples

---
+++ redux-app/app/store/configureStore.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'react-redux';
+import reducer from '../reducers';
+
+const createStoreWithMiddleware = applyMiddleware(
+  thunk
+)(createStore);
+
+export default function configureStore(initialState) {
+  const store = createStoreWithMiddleware(reducer, initialState);
+
+  return store;
+}
---
+++ redux-app/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+};
